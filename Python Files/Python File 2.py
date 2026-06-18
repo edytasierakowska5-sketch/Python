2509e06f25317266d3c7117c9d691d9818395aab
GITHUB EXPORTER <edytasierakowska5@gmail.com>
2026-06-18 14:23:20 +0200

    Add input handling for list number validation

--- Python Files/Python File 2.py
+++ Python Files/Python File 2.py
@@ -1,1 +1,31 @@
+cyfry = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
+result = str(list(cyfry))
+
+print(result)
+Text = input("Wpisz Numer listy jest prawidłowy: ")
+
+if Text == "1":
+    print("Inny Numer")
+elif Text == "2":
+    print("Inny Numer")
+elif Text == "3":
+    print("Inny Numer")
+elif Text == "4":
+    print("Inny Numer")
+elif Text == "5":
+    print("Inny Numer")
+elif Text == "6":
+    print("Inny Numer")
+elif Text == "7":
+    print("Inny Numer")
+elif Text == "8":
+    print("Inny Numer")
+elif Text == "9":
+    print("Inny Numer")
+elif Text == "10":
+    print("Koniec Numeru")
+elif Text == "":
+    print("Numer Listy jest Nie Prawidłowy ten: 0")
+else:
+    print("Numer Listy jest Nie Prawidłowy ten: " + Text)
